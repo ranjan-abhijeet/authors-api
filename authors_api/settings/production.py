72985a83c1c718485e6d09c83c011ab628562a2b
ranjan-abhijeet <ranjan.viper@outlook.com>
2023-12-05 12:37:00 +0530

    setup basic project structure.

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [("Abhijeet Ranjan", "ranjan.viper@outlook.com")]
+
+# ToDo: add domain name of production server
+CSRF_TRUSTED_ORIGINS = [""]
---
+++ core_apps/profiles/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy
+
+class ProfilesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core_apps.profiles"
+    verbose_name = gettext_lazy("Profiles")
